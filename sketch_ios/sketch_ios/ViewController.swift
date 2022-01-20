d0e581bfef4e9cb4a541b824b3a7ccc4261f635d
田 知勲 <j-jihoon@winas.jp>
2022-01-20 16:09:26 +0900

    first commit

---
+++ sketch_ios/sketch_ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sketch_ios
+//
+//  Created by 전지훈 on 2022/01/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
